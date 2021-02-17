3b2fe8ff9dbefeb50fae7cdd9ff3a926c97c3353
Payatoff Nurbek <75463544+Paatoff-Nurbek@users.noreply.github.com>
2021-02-18 01:46:38 +0600

    initial commit

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
